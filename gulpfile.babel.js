061671479cb8045ea816955a88c9c2caffb2ce98
Matt Thomas <matthew.thomas@centare.com>
2016-05-21 14:45:28 -0500

    Gulp
    Setup Gulp to run the presentation.

---
+++ gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp'
+import connect from 'gulp-connect'
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    })
+})
+
+gulp.task('slides', function() {
+    gulp.src('index.html')
+        .pipe(connect.reload());
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['index.html', 'js/index.js'], ['slides']);
+})
+
+gulp.task('default', ['connect', 'watch']);
---
+++ js/index.js
@@ -0,0 +1,20 @@
+// More info https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+    history: true,
+    progress: false,
+        // More info https://github.com/hakimel/reveal.js#dependencies
+    dependencies: [{
+        src: 'plugin/markdown/marked.js'
+    }, {
+        src: 'plugin/markdown/markdown.js'
+    }, {
+        src: 'plugin/notes/notes.js',
+        async: true
+    }, {
+        src: 'plugin/highlight/highlight.js',
+        async: true,
+        callback: function() {
+            hljs.initHighlightingOnLoad();
+        }
+    }]
+});
