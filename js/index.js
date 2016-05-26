237ba681beda2268fd9188a5b74a37a91256aa45
Matt Thomas <matthew.thomas@centare.com>
2016-05-25 21:02:14 -0500

    Sections
    let, const, var and no semicolons

--- js/index.js
+++ js/index.js
@@ -2,18 +2,9 @@
 Reveal.initialize({
     history: true,
     progress: false,
-    center: false,
-    width: "95%",
-    height: "95%",
+    width: '90%',
         // More info https://github.com/hakimel/reveal.js#dependencies
     dependencies: [{
-        src: 'plugin/markdown/marked.js'
-    }, {
-        src: 'plugin/markdown/markdown.js'
-    }, {
-        src: 'plugin/notes/notes.js',
-        async: true
-    }, {
         src: 'plugin/highlight/highlight.js',
         async: true,
         callback: function() {
