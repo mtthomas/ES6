0fb30f5e263b0eedef73798a858f3b02f8a5b751
Matt Thomas <matthew.thomas@centare.com>
2016-05-27 10:08:52 -0500

    Loading external slides
    Loading slides from HTML files

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
-// More info https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-    history: true,
-    progress: false,
-    width: '90%',
+
+    // var $slides = $("#slides"),
+    //     index = 0,
+    //     sections = [],
+    //     deferreds = []
+    //
+    // for (index = 0; index < slides.length; index += 1) {
+    //     sections.push('');
+    // }
+    //
+    // deferreds = slides.map(function(slide, index) {
+    //     return $.get('slides/' + slide + '.html').done(function(data) {
+    //         sections[index - 1] = data;
+    //     });
+    // });
+    //
+    // $.when.apply($, deferreds).then(function() {
+    //     $slides.append(sections)
+    // }, function() {
+    //     console.log('failed to load the slides');
+    // }).done(function() {
+    //     // More info https://github.com/hakimel/reveal.js#configuration
+    //
+    // });
+
+    Reveal.initialize({
+        history: true,
+        progress: false,
+        width: '90%',
         // More info https://github.com/hakimel/reveal.js#dependencies
-    dependencies: [{
-        src: 'plugin/highlight/highlight.js',
-        async: true,
-        callback: function() {
-            hljs.initHighlightingOnLoad();
-        }
-    }]
-});
+        dependencies: [{
+            src: 'plugin/highlight/highlight.js',
+            async: false,
+            callback: function() {
+                hljs.initHighlightingOnLoad();
+            },
+        }, {
+            src: 'plugin/external/external.js',
+            condition: function() {
+                return !!document.querySelector('[data-external]');
+            }
+        }]
+    });
