a32c342a476a641215c5bd49ba717fea9f4d00ae
Matt Thomas <matthew.thomas@centare.com>
2016-06-14 19:51:50 -0500

    Arrow
    Created an arrow section and started on classes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 Reveal.initialize({
     width: 1366,
     height: 768,
+    margin: 0.1,
+    center: false,
+    progress: false,
+    slideNumber: true,
     dependencies: [
         // Syntax highlight for <code> elements
         {
